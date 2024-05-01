b3c2ca0469f694f640a352ab46c53b3ebc833a51
K. Manmohan Reddy <manmohanreddyk.999@gmail.com>
2024-05-01 01:10:43 -0500

    added carmake and carmodel models

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -19,7 +19,7 @@ class CarModel(models.Model):
     CAR_TYPES = [('SEDAN', 'sedan'), ('SUV', 'Suv'), ('WAGON', 'Wagon'), 
                  ('HATCHBAG', 'Hatchbag'), ('SPORTS', 'Sports'), ('CROSSOVER', 'Crossover')]
     type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023, validators=[MaxValueValidator(2023), MinValueValidator(2015)]),
+    year = models.IntegerField(default=2023, validators=[MaxValueValidator(2023), MinValueValidator(2015)])
 
     def __str__(self):
         return self.name
